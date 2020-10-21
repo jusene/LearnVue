8a745f447840ca40d743b4f0952dba7fa5b26e5c
jusene <710800324@qq.com>
2020-10-21 23:58:35 +0800

    vue

--- day4/README.md
+++ day4/README.md
@@ -17,3 +17,9 @@ vue cli2初始化项目
 
 vue cli3初始化项目
 - vue create my-project
+
+> runtime-compiler(v1)
+- template -> ast -> render -> vdom -> UI
+
+> runtime-only(v2)(1.性能更少 2.性能更强)
+- render -> vdom -> UI
---
+++ day4/runningtimecli2/src/main.js
@@ -0,0 +1,14 @@
+import Vue from 'vue'
+import App from './App'
+
+Vue.config.productionTip = false
+
+/* eslint-disable no-new */
+new Vue({
+  el: '#app',
+  render: h => h(App)
+  // createElement('标签', {标签属性}, [''])
+  // render: function(createElement) {
+  //  return createElement('h2', {class: 'box'}, ['hello world'])
+  //}
+})
