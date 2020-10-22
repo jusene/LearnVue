61857263ff6a769bedcbbeac229409744e4d5b63
jusene <710800324@qq.com>
2020-10-22 22:07:20 +0800

    vue

---
+++ day4/learnvuerouter2/src/components/AboutMe.vue
@@ -0,0 +1,20 @@
+<template>
+    <h1>{{name}}</h1>
+</template>
+
+<script>
+export default {
+    data() {
+        return {
+            name: 'jusene'
+        }
+    }
+}
+</script>
+
+<style scoped>
+h1 {
+    font-size: 50px;
+    font-family: 'Courier New', Courier, monospace;
+}
+</style>
