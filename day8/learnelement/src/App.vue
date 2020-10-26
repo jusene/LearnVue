001749ddad89f4452b9189d7853a6def32f2929d
Administrator <admin@example.com>
2020-10-26 18:23:38 +0800

    vue

---
+++ day8/learnelement/src/App.vue
@@ -0,0 +1,27 @@
+<template>
+  <div id="app">
+    <el-row>
+      <el-button @click="btnClick">默认按钮</el-button>
+      <el-button type="primary">主要按钮</el-button>
+    </el-row>
+  </div>
+</template>
+
+<script>
+
+export default {
+  name: 'App',
+  methods: {
+    btnClick() {
+      this.$message({
+        type: 'warning',    
+        message: "消息"
+        })
+    }
+  }
+}
+</script>
+
+<style>
+
+</style>
