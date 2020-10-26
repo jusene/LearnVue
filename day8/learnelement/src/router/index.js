d8577c8e67646f9449c9d840c0f044410ccbc9af
jusene <710800324@qq.com>
2020-10-26 22:18:05 +0800

    vue

---
+++ day8/learnelement/src/router/index.js
@@ -0,0 +1,24 @@
+import Vue from 'vue'
+import Router from 'vue-router'
+
+Vue.use(Router)
+
+const Login = () => import('@/views/login/index')
+
+/* 通用路由 */
+export default new Router = ({
+    routes: [
+        {
+            path: '',
+            redirect: '/login'
+        },
+        {
+            path: '/login',
+            name: 'Login',
+            component: Login,
+            meta: {
+                title: '登陆'
+            }
+        }   
+    ]
+})
---
+++ day8/learnelement/src/views/login/index.vue
@@ -0,0 +1,38 @@
+<template>
+    <div class="login">
+    <div class="loginBox">
+        <h2 class="loginH2"><strong>运维后台系统</strong></h2>
+    </div>
+    </div>
+</template>
+
+<script>
+export default {
+    name: "Login"
+}
+</script>
+
+<style scoped>
+.login {
+    height: 100%;
+    width: 100%;
+    background: url(../../assets/pageBg/loginBg.jpg) no-repeat center center;
+    background-size: 100% 100%;
+    overflow: hidden;
+}
+
+.loginBox {
+    height: 455px;
+    width: 550px;
+    margin: 0 auto;
+    position: relative;
+    top: 50%;
+    margin-top: -287px;
+}
+
+.loginH2 {
+  font-size: 38px;
+  color: #fff;
+  text-align: center;
+}
+</style>
