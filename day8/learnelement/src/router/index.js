20fb151bd4a8a1411d5827c405dad515343f43c4
jusene <710800324@qq.com>
2020-10-27 21:17:58 +0800

    vue

--- day8/learnelement/src/router/index.js
+++ day8/learnelement/src/router/index.js
@@ -3,10 +3,11 @@ import Router from 'vue-router'
 
 Vue.use(Router)
 
-const Login = () => import('@/views/login/index')
+const Login = () => import('@/views/login')
 
 /* 通用路由 */
-export default new Router = ({
+export default new Router({
+    mode: 'history',
     routes: [
         {
             path: '',
