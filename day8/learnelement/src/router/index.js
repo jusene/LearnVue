9af88d3b6d0b758242233df23f2ace0834ac38ae
jusene <710800324@qq.com>
2020-10-27 23:39:26 +0800

    vue

--- day8/learnelement/src/router/index.js
+++ day8/learnelement/src/router/index.js
@@ -6,12 +6,8 @@ Vue.use(Router)
 const Login = () => import('@/views/login')
 
 /* 通用路由 */
-<<<<<<< HEAD
 export default new Router({
     mode: 'history',
-=======
-export default new Router ({
->>>>>>> a9af3ef73bc7b34a9bd632485787abe903bfd651
     routes: [
         {
             path: '',
